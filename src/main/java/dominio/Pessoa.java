8b73ebeca2a0696b80ded154b7d29d202c947b44
Daniel Junior <danieljunior1903@gmail.com>
2024-03-22 19:34:16 -0400

    Inserting date at database

--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -2,12 +2,25 @@ package dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
 public class Pessoa implements Serializable {
 	
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name="nomecompleto")
 	private String nome;
 	private String email;
 	
